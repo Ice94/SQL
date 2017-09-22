094c6b982e67b35cdd2e7d1e0408583fe2ed76c5
Ice94 <mhbratek@gmail.com>
2017-09-22 23:43:58 +0200

    Finish querydata tutorial, also start working on filtering data part.

--- QueryingData.sql
+++ QueryingData.sql
@@ -3,3 +3,13 @@ select * from employees;
 select distinct * from  employees;
 select lastName from employees order by lastName;
 select distinct lastName from employees order by lastName;
+
+select distinct state, city from customers where state is not null order by state, city;
+
+select state from customers group by state;
+
+select distinct state from customers;
+
+select count(distinct state) from customers where country = 'USA';
+
+select distinct state from customers where state is not null limit 5;
