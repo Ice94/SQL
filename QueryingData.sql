51c95af849b5c9df9a393b04997ca5f4303fec53
Ice94 <mhbratek@gmail.com>
2017-09-22 23:35:27 +0200

    Querying data ex.

---
+++ QueryingData.sql
@@ -0,0 +1,5 @@
+select lastName, firstName, jobTitle from employees;
+select * from employees;
+select distinct * from  employees;
+select lastName from employees order by lastName;
+select distinct lastName from employees order by lastName;
